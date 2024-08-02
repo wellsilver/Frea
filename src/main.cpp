a803760dc64af1b31da5fbbf28d8074899571a38
wellsilver <wellsilver2016@gmail.com>
2024-08-02 01:52:24 -0400

    will now close correctly instead of sigint

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,18 @@ int active = 1;
 #include <thread>
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 
 #include "socket.hpp"
 
+void intHandler(int dummy) {
+  printf("Stopping\n");
+  active = 0;
+}
+
 int main(int argc, char **argv) {
+  signal(SIGINT, intHandler);
+
   pthread_t socketthread;
   pthread_create(&socketthread, NULL, socketmain, NULL);
 
